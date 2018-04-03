e82f852b2e7aafeabd9b37c3c78dc096d5aaf707
Joshua <joshuaprpich@gmail.com>
2018-04-02 19:45:04 -0500

    first commit

---
+++ test.js
@@ -0,0 +1,102 @@
+// function count(str, char){
+//   var counter = 0
+//   for (var i = 0; i < str.length; i++) {
+//     if (str[i] === char) {
+//       counter ++;
+//     }
+//   }
+//   return counter
+// }
+
+// console.log(count("Helloll", "l")); // retorna 2
+
+
+// function filter(arr){
+//   var newArr = [];
+//   for (var i = 0; i < arr.length; i++) {
+//     if(arr[i] > 10) {
+//       newArr.push(arr[i])
+//     }
+//   }
+//   return newArr;
+// }
+
+
+// console.log(filter([3, 12, 45, 7])); // retorna [12, 45]
+// console.log(filter([3, 5]));         // retorna []
+
+
+
+function hypotenuse(a,b){
+  var result = Math.sqrt((a * a) + (b * b));
+  return result;
+}
+
+console.log(hypotenuse(3,4))
+
+
+console.log(hypotenuse(3, 4));    // retorna 5
+console.log(hypotenuse(10, 13));  // retorna 16.401219466856727
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
